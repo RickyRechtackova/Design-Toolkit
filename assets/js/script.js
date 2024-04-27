a74efba0d0c35599569a6fd558a83ef089337fe1
Rachel <c00284406@itcarlow.ie>
2024-04-27 20:49:02 +0100

    logo text, icon dark and light

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,15 @@ function hamburger() {
         x.className = "nav-links";
     }
 }
+
+// dark-mode media query matched or not
+let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+if(matched){
+	console.log('Currently in dark mode'); 
+    document.getElementById("browserIcon").href="assets/img/icon.ico"
+}
+else{
+	console.log('Currently not in dark mode');
+    document.getElementById("browserIcon").href="assets/img/iconBlack.ico"
+}
