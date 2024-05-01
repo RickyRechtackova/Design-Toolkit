e9febf1ee2b8d328d373e0a5154f03c7a4c2b278
Ricky <erikarech.01@gmail.com>
2024-05-01 17:20:06 +0200

    figma tool done

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,64 @@ else{
 	console.log('Currently not in dark mode');
     document.getElementById("browserIcon").href="assets/img/iconBlack.ico"
 }
+
+// js for tools page
+
+let beginnerButton = document.getElementById("beginner");
+let intermediateButton = document.getElementById("intermediate");
+let masterButton = document.getElementById("master");
+
+let beginnerSectionFigma = document.getElementById("beginner-figma")
+let intermediateSectionFigma = document.getElementById("intermediate-figma")
+let masterSectionFigma = document.getElementById("master-figma")
+
+beginnerButton.addEventListener("click", displayBeginner);
+intermediateButton.addEventListener("click", displayIntermediate);
+masterButton.addEventListener("click", displayMaster)
+
+function displayBeginner(){
+    beginnerButton.classList.remove("level1");
+    beginnerButton.classList.add("active-level");
+
+    intermediateButton.classList.add("level2");
+    intermediateButton.classList.remove("active-level");
+
+    masterButton.classList.add("level3");
+    masterButton.classList.remove("active-level");
+
+    beginnerSectionFigma.style.display = "grid";
+    intermediateSectionFigma.style.display = "none";
+    masterSectionFigma.style.display = "none";
+    console.log(beginnerSectionFigma.style.display)
+}
+
+function displayIntermediate(){
+    beginnerButton.classList.add("level1")
+    beginnerButton.classList.remove("active-level")
+
+    intermediateButton.classList.remove("level2")
+    intermediateButton.classList.add("active-level")
+
+    masterButton.classList.add("level3")
+    masterButton.classList.remove("active-level")
+
+    beginnerSectionFigma.style.display = "none";
+    intermediateSectionFigma.style.display = "grid"
+    masterSectionFigma.style.display = "none"
+
+}
+
+function displayMaster(){
+    beginnerButton.classList.add("level1")
+    beginnerButton.classList.remove("active-level")
+
+    intermediateButton.classList.add("level2")
+    intermediateButton.classList.remove("active-level")
+
+    masterButton.classList.remove("level3")
+    masterButton.classList.add("active-level")
+
+    beginnerSectionFigma.style.display = "none";
+    intermediateSectionFigma.style.display = "none"
+    masterSectionFigma.style.display = "grid"
+}
